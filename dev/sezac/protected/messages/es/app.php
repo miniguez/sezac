923c9cc18d1342d37a468f663a141c93da5cf2ba
Miguel Angel <miniguez@MacBook-Pro-de-Miguel.local>
2015-06-07 13:06:58 -0500

    Edito diagrama de base de datos

--- dev/sezac/protected/messages/es/app.php
+++ dev/sezac/protected/messages/es/app.php
@@ -3,8 +3,11 @@
 return array(
     'Fields with'=>'Campos con ',
     'are required.'=>' son requeridos',
+    'Mensaje_Confirmar_Aceptar'=>'¿Desea eliminar el registro?',
     'Update'=>'Actualizar',
     'Delete'=>'Eliminar',
     '_CATALOGOS'=>'Catálogos',
     '_DEPENDENCIAS'=>'Dependencias',
+    '_UNIDADESRESPONSABLES'=>'Unidades responsables',
+    '_ANIOSFISCALES'=>'Años fiscales'
  );
